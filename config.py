558bb1467d65674ca05335af52b244ec12a08c19
GenshinBot <109652760+genshin-discord@users.noreply.github.com>
2022-07-20 15:51:24 +0800

    v0.1

---
+++ config.py
@@ -0,0 +1,15 @@
+# token from discord
+DISCORD_BOT_TOKEN = ''
+
+# database config
+MYSQL_USER = 'root'
+MYSQL_PASS = 'yourpassword'
+MYSQL_HOST = '127.0.0.1'
+MYSQL_PORT = '3306'
+MYSQL_DB = 'bot'
+
+BOT_ID = your bot id
+BOT_MASTER = your discord id
+
+# Not used
+TEST_GUILDS = []
