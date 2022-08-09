ed439f7c5115913960b7d97d184b61cc95476485
GenshinBot <109652760+genshin-discord@users.noreply.github.com>
2022-08-09 16:18:06 +0800

    Add files via upload

--- constant.py
+++ constant.py
@@ -4,3 +4,14 @@ Please login in https://bbs.mihoyo.com/ys/ or https://www.hoyolab.com/ and send 
 NOTE: Your cookies **MUST HAVE** "cookie_token" otherwise it cannot be used.
 Try use incognito mode/inprivate mode if your cookies doesn't have "cookie_token"
 Check https://github.com/genshin-discord/genshinbot/wiki/GenshinBot-registration-help for more detailed steps'''
+
+name_map = {'神里绫华': 'Kamisato Ayaka', '迪卢克': 'Diluc', '温迪': 'Venti', '可莉': 'Klee', '钟离': 'Zhongli', '达达利亚': 'Tartaglia',
+            '七七': 'Qiqi', '甘雨': 'Ganyu', '莫娜': 'Mona', '胡桃': 'Hu Tao', '枫原万叶': 'Kaedehara Kazuha',
+            '雷电将军': 'Raiden Shogun', '珊瑚宫心海': 'Sangonomiya Kokomi', '八重神子': 'Yae Miko', '夜兰': 'Yelan', '申鹤': 'Shenhe',
+            '香菱': 'Xiangling', '行秋': 'Xingqiu', '菲谢尔': 'Fischl', '班尼特': 'Bennett', '罗莎莉亚': 'Rosaria',
+            '九条裟罗': 'Kujou Sara', '荒泷一斗': 'Arataki Itto', '迪奥娜': 'Diona', '砂糖': 'Sucrose', '五郎': 'Gorou', '雷泽': 'Razor',
+            '宵宫': 'Yoimiya', '旅行者': 'Traveler', '芭芭拉': 'Barbara', '凝光': 'Ningguang', '刻晴': 'Keqing', '丽莎': 'Lisa',
+            '安柏': 'Amber', '烟绯': 'Yanfei', '琴': 'Jean', '重云': 'Chongyun', '凯亚': 'Kaeya', '诺艾尔': 'Noelle',
+            '北斗': 'Beidou', '辛焱': 'Xinyan', '埃洛伊': 'Aloy', '早柚': 'Sayu', '托马': 'Thoma', '云堇': 'Yun Jin',
+            '久岐忍': 'Kuki Shinobu', '鹿野院平藏': 'Heizou', '神里绫人': 'Ayato', '优菈': 'Eula', '魈': 'Xiao',
+            '阿贝多': 'Albedo'}
